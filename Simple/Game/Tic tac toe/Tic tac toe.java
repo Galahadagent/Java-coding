e6247c34fbcef04a8d7011cf419be50f57ffcbd0
Galahadagent <161305918+Galahadagent@users.noreply.github.com>
2024-05-26 22:30:28 +0530

    Update Tic tac toe.java
    V0.2 (#5)

--- Simple/Game/Tic tac toe/Tic tac toe.java
+++ Simple/Game/Tic tac toe/Tic tac toe.java
@@ -35,7 +35,7 @@ public class TicTacToe {
     for (int i = 0; i < 3; i++) {
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
             return true;
-        }
+            }
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
             return true;
         }
